b0e5df5594c57391883700fbf97687166d5ffaa7
kalelek <netlelo@live.com>
2014-11-21 20:23:39 +0100

    Dodane funkcje wirtualne do klasy pochodnej dodana uwaga co  do biblioteki czasu

--- blum_generator.cpp
+++ blum_generator.cpp
@@ -2,19 +2,33 @@
 
 Blum_Generator::Blum_Generator():prime_number_1(167),prime_number_2(179),base_modulo(prime_number_1*prime_number_2),min(0),max(10000)
 {
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    long seed = std::chrono::system_clock::now().time_since_epoch().count();
     current_random_number=seed;
 }
 
-unsigned long Blum_Generator::to_draw_number()
+double Blum_Generator::to_draw_number()
 {
     long next_random_number;
     next_random_number=((current_random_number*current_random_number)%base_modulo);
     current_random_number=next_random_number;
-    return current_random_number%max+min; /*????????? czy to nie popsuje algorytmu*/
-    //return current_random_number;
+    return (double)current_random_number/(base_modulo-1);
 }
-unsigned long operator()()
+double operator()()
 {
-    return to_draw_number()
+    return to_draw_number();
+}
+double losujLiczbe()
+{
+    return to_draw_number();
+}
+std::vector<double> losujTablice(int n)
+{
+    std::vector<double> vek(n);
+	for(int i=0; i<vek.size(); i++)
+		wek[i]=to_draw_number();
+	return vek;
+}
+virtual void setSeed(double newSeed)
+{
+    current_random_number=newSeed;
 }
