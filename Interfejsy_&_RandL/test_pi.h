b6b209907f267c72e98772f073099fe9aff86980
Łukasz Radzio <radluki@gmail.com>
2014-11-21 22:07:25 +0100

    test_pi razem z programem testowym

---
+++ Interfejsy_&_RandL/test_pi.h
@@ -0,0 +1,22 @@
+//test pi
+//autor - Łukasz Radzio
+#ifndef TEST_PI
+#define TEST_PI
+#include"Test.h"
+
+class test_pi:public Test
+{
+public:
+	test_pi(int ile_losowac=10000):ile(ile_losowac){};//ile punktow ma losowac test
+	void testuj(Generator *gener);
+	double wynik();
+	double interpretacja();
+	~test_pi(){};
+private:
+	double przyblizenie_pi;
+	double error;
+	int ile;
+};
+
+
+#endif //TEST_PI
