52e0677e1c5fbd119be68ba6de29c8f8cf336ab9
kalelek <netlelo@live.com>
2014-11-21 14:52:56 +0100

    program do znalezienia odpowiednich liczb pierwszych do generatora Blum

---
+++ FindAdequatePrime.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(void)
+{
+
+        int  N;
+        std::cin  >>  N;
+        for (int i =  2;  N > 0;  ++i)
+        {
+            bool  isPrime  =  true ;
+            for (int  j =  2;  j < i;  ++j)
+            {
+                if (i  % j ==  0)
+                {
+                    isPrime  =  false ;
+                    break ;
+                }
+            }
+            if (isPrime)
+            {
+                --N;
+                if((i % 4 )==3)
+                {
+                    std::cout  <<  i  <<  "\n";
+                }
+            }
+        }
+    return 0;
+}
