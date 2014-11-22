90eee1bfabff47e53e21fac196a65d85c3c5e8fa
Łukasz Radzio <radluki@gmail.com>
2014-11-22 19:52:53 +0100

    Usunięcie możliwości wylosowania ujemnych warunków początkowych

--- losuj_parametr.m
+++ losuj_parametr.m
@@ -1,4 +1,9 @@
 function y=losuj_parametr(E,delta)
-%% LOSUJ_PARAMETR(E,delta) zwraca wartość z przedziału [E-delta,E+delta]
-y=E + 2*(rand(1)-0.5)*delta;
+%% LOSUJ_PARAMETR(E,delta) 
+% zwraca wartość z przedziału [E-delta,E+delta], gdy E-delta>0
+% inaczej losuje z [0,E+delta]
+if E>delta
+    y=E + 2*(rand(1)-0.5)*delta;
+else
+    y=rand(1)*(E+delta);
 end
