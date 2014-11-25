f2afa0b6f25842f4864e78c9fb199ccc3a94d1f6
Łukasz Radzio <radluki@gmail.com>
2014-11-25 12:06:51 +0100

    model1.m tak można zrobić funkcje z bazy danych

---
+++ model1.m
@@ -0,0 +1,5 @@
+function [fun,names,default_values] = model1()
+% Mniejwięcej tak można zrobić funkcje z bazy danych
+fun = @(param) [param(3)-param(2);param(1)*param(2)];
+names={'a','b','c'}
+default_values=[1 2 3]
